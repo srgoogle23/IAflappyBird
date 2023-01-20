87dbec42b24c21a70c9ad2cc67536d3b41c1ff0d
Leonardo Oliveira <leooli.teixeira@gmail.com>
2023-01-19 21:31:03 -0300

    fix: reformatado classe

--- jogo/cano.js
+++ jogo/cano.js
@@ -1,42 +1,40 @@
 class Cano {
-    constructor() {
-  
-      /**
+    constructor() { /**
        * Tamanho do espaço vazio entre os canos
        */
-      let tamanho = 125;
-  
-      /**
+        let tamanho = 125;
+
+        /**
        * Centro do espaço vazio entre os canos
        */
-      let centro = random(tamanho, altura - tamanho);
-  
-      /**
+        let centro = random(tamanho, altura - tamanho);
+
+        /**
        * Altura do topo do cano
        */
-      this.topo = centro - tamanho / 2;
-  
-      /**
+        this.topo = centro - tamanho / 2;
+
+        /**
        * Altura do fundo do cano
        */
-      this.baixo = altura - (centro + tamanho / 2);
-  
-      /**
+        this.baixo = altura -(centro + tamanho / 2);
+
+        /**
        * Posição do cano
        */
-      this.x = largura;
-  
-      /**
+        this.x = largura;
+
+        /**
        * Largura do cano
        */
-      this.w = 80;
-  
-      /**
+        this.w = 80;
+
+        /**
        * Velocidade do cano
        */
-      this.velocidade = 6;
+        this.velocidade = 6;
     }
-  
+
     /**
      * Verifica se o passáro bateu no cano
      * 
@@ -44,40 +42,39 @@ class Cano {
      * @returns 
      */
     bateu(passaro) {
-      if ((passaro.y - passaro.r) < this.topo || (passaro.y + passaro.r) > (altura - this.baixo)) {
-        if (passaro.x > this.x && passaro.x < this.x + this.w) {
-          return true;
+        if ((passaro.y - passaro.r) < this.topo || (passaro.y + passaro.r) > (altura - this.baixo)) {
+            if (passaro.x > this.x && passaro.x < this.x + this.w) {
+                return true;
+            }
         }
-      }
-      return false;
+        return false;
     }
-  
+
     /**
      * Mostra o cano
      */
     mostrar() {
-      image(pipeBodySprite, this.x, 0, this.w, this.topo);
-      image(pipePeakSprite, this.x, altura - this.baixo, this.w, this.baixo);
+        image(pipeBodySprite, this.x, 0, this.w, this.topo);
+        image(pipePeakSprite, this.x, altura - this.baixo, this.w, this.baixo);
     }
-  
+
     /**
      * Atualiza o cano
      */
     atualizar() {
-      this.x -= this.velocidade;
+        this.x -= this.velocidade;
     }
-  
+
     /**
      * Se o cano saiu da tela, retorna true, senão, retorna false
      * 
      * @returns boolean
      */
     saiuDaTela() {
-      if (this.x < -this.w) {
-        return true;
-      } else {
-        return false;
-      }
+        if (this.x < -this.w) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
-  
+}
